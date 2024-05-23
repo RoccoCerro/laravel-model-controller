f6709d1d12591f49960928e067882b8701afce6f
RoccoCerro <rcerro02@gmail.com>
2024-05-23 15:23:57 +0200

    Add: content in the card

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -6,7 +6,13 @@
       @foreach ($movie as $obj)
       <div class="col-3">
         <div class="card">
-          {{$obj->title}}
+          <ul class="list-unstyled">
+            <li>{{$obj->title}}</li>
+            <li>{{$obj->original_title}}</li>
+            <li>{{$obj->nationality}}</li>
+            <li>{{$obj->date}}</li>
+            <li>{{$obj->vote}}</li>
+          </ul>
         </div>
       </div>
       @endforeach
